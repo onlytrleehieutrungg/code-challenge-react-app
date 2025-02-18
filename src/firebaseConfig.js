d176f2ee71f5fe1222e4af63d1d94791b56e3124
Steven Tran <trungtran2k01@gmail.com>
2025-02-18 16:41:55 +0700

    deploy

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAY4cL61dfaBsR8_WybDFRtBb-VWn9RH3A",
+  authDomain: "deploy-vite-react-app.firebaseapp.com",
+  projectId: "deploy-vite-react-app",
+  storageBucket: "deploy-vite-react-app.firebasestorage.app",
+  messagingSenderId: "391752589453",
+  appId: "1:391752589453:web:344f0dcb429e426ef0c2c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
